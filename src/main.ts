c42aa532d885410508fd2a2bed929eb86882bfd4
Kenta Kawabata <kenjuly67@gmail.com>
2026-01-07 00:35:51 +0900

    タイプスクリプトで電卓を作成しました

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-// import './style.css'
+import './style.css'
 
 
 // // コーディングルール
@@ -15,254 +15,6 @@
 // // ③各演算子を押した際の処理を実装
 // //   numberに変換
 
-
-// // ディスプレイの要素の取得
-// const $display = document.querySelector("#display");
-
-// // ボタン要素の取得
-// const $btn1 = document.querySelector("#btn1");
-// const $btn2 = document.querySelector("#btn2");
-// const $btn3 = document.querySelector("#btn3");
-// const $btn4 = document.querySelector("#btn4");
-// const $btn5 = document.querySelector("#btn5");
-// const $btn6 = document.querySelector("#btn6");
-// const $btn7 = document.querySelector("#btn7");
-// const $btn8 = document.querySelector("#btn8");
-// const $btn9 = document.querySelector("#btn9");
-// const $btn0 = document.querySelector("#btn0");
-
-// // Cボタン = ボタンの取得
-// const $btnClear = document.querySelector("#clear");
-// const $btnEqual = document.querySelector("#equal");
-
-// // 演算子の取得
-// const $btnMinus = document.querySelector("#minus");
-// const $btnDivide = document.querySelector("#divide");
-// const $btnMultiple = document.querySelector("#multiple");
-// const $btnPlus = document.querySelector("#plus");
-
-
-// // 初期値の設定
-// let currentValue = "0"; //入力中の数字
-// let previousValue = null; //演算をを押したらcurrentValueをこちらに移す
-// let operator = null; //演算子の初期値
-// let justCBtn = false; // C直後 
-
-// // 数字を押したときの処理
-// function inputNumber(num) {
-//   //8桁目を制限
-//   const numOnlyLen = currentValue.replace(".", "").replace("-", "");
-//   if (numOnlyLen.length >= 8)  
-//     return;
-//   if(currentValue === "0") {
-//     currentValue = num; //初めて数字のボタンを押した時は０からそのボタンに変更する
-//     justCBtn = false;
-//   } else {
-//     currentValue += num; //2回目以降は押したボタンを追加していく
-//   }
-
-//   $display.value = currentValue; //表示画面のを入力中にする
-
-// }
-
-// //各のボタンにイベントを割り当てる
-// // 例：1を押した※文字列としての1を取得
-// //※+=の処理を　11 のように実行するため (計算実行前にnumberへ変換は後に記載)
-
-// $btn1.addEventListener("click",() => {
-//   inputNumber("1");
-// });
-
-// $btn2.addEventListener("click",() => {
-//   inputNumber("2")
-// });
-
-// $btn3.addEventListener("click",() => {
-//   inputNumber("3")
-// });
-
-// $btn4.addEventListener("click",() => {
-//   inputNumber("4")
-// });
-
-// $btn5.addEventListener("click",() => {
-//   inputNumber("5")
-// });
-
-// $btn6.addEventListener("click",() => {
-//   inputNumber("6")
-// });
-
-// $btn7.addEventListener("click",() => {
-//   inputNumber("7")
-// });
-
-// $btn8.addEventListener("click",() => {
-//   inputNumber("8")
-// });
-
-// $btn9.addEventListener("click",() => {
-//   inputNumber("9")
-// });
-
-// $btn0.addEventListener("click",() => {
-//   inputNumber("0")
-// });
-
-// //演算処理
-// // ①previousValueが空白→currentValueを移す
-// // ②両方ある場合→number型に変換し計算
-// // ③operatorを更新
-// // ④currentValueをリセット
-
-// function handleOperator (op) {
-
-//   if (justCBtn && op !== "-") {
-//     return;
-//   }
-
-//   if (justCBtn && op === "-") {
-//     currentValue = "-";
-//     $display.value = currentValue
-//     return;
-//   }
-
-//   if (previousValue === null && currentValue === "0" && op !== "-") {
-//     return;
-//   }
-
-//   if (previousValue === null && currentValue === "0" && op === "-") {
-//     currentValue = "-";
-//     $display.value = currentValue;
-//     return;
-//   }
-
-//   if (previousValue !== null && currentValue === "0") {
-//     operator = op;
-//     return;
-//   }
-
-//   // previousValueが空白ではない　かつ　currentValueが0ではない場合の処理
-//   if (previousValue !== null && currentValue !== "0") {
-//     // 入力中に文字列としての数字と演算子を押す前の文字列の数字をnumberに変換
-//     const a = Number(previousValue);
-//     const b = Number(currentValue);
-
-//     //演算子の処理
-//     if (operator === "+") previousValue = a + b;
-//     if (operator === "-") previousValue = a - b;
-//     if (operator === "*") previousValue = a * b;
-//     if (operator === "/") previousValue = a / b;
-//   } else {
-//     previousValue = Number(currentValue);
-//   }
-
-//   operator = op; //演算子は何かを記録
-//   currentValue = "0"; //入力中の数字を０にしてリセット
-//   $display.value = previousValue; //計算途中の結果を出す
-// }
-
-// // 演算子のイベント
-// $btnPlus.addEventListener("click", () => {
-//   handleOperator("+")
-// });
-
-// $btnMinus.addEventListener("click", () => {
-//   handleOperator("-")
-// });
-
-// $btnMultiple.addEventListener("click", () => {
-//   handleOperator("*")
-// });
-
-// $btnDivide.addEventListener("click", () => {
-//   handleOperator("/")
-// });
-
-
-// // =の処理
-
-// $btnEqual.addEventListener("click", () => {
-
-//   if (previousValue !== null && operator !== null && currentValue === "0") {
-//     return;
-//   }
-//   if (previousValue !== null && operator !== null) {
-//     const a = Number(previousValue);
-//     const b = Number(currentValue);
-
-//      //0で割るとdisplayにエラーを表示
-//     if (operator === "/" && b === 0) {
-//       $display.value = "エラー";
-//       currentValue = "0";
-//       previousValue = null;
-//       operator = null;
-//       return;
-//     }
-
-//     let result = 0;
-//     if (operator === "+") result = a + b;
-//     if (operator === "-") result = a - b;
-//     if (operator === "*") result = a * b;
-//     if (operator === "/") result = a / b;
-
-//     //指数表示の処理
-//     const bigIntNum = String(result).replace(".", "").replace("-", "").length;
-    
-//     if (bigIntNum > 8) {
-//       const ex = result.toExponential(7);
-//       $display.value = ex;
-
-//     currentValue = String(result);
-//     previousValue = null;
-//     operator = null;
-//     return;
-//     }
-
-//     $display.value = result;
-
-//     // 次の計算に備える
-//     currentValue = String(result);
-//     previousValue = null;
-//     operator = null;
-//   }
-// });
-
-
-// // Cの処理
-// $btnClear.addEventListener("click", () => {
-//   currentValue = "0";
-//   previousValue = null;
-//   operator = null;
-//   justCBtn = true;
-//   $display.value = "0";
-// });
-
-// //小数点の処理
-// const $btnDot = document.querySelector("#dot");
-
-// $btnDot.addEventListener("click",() =>{
-
-//     if (justCBtn) {
-//       return;
-//     }
-
-//     if(currentValue.includes(".")){
-//       return;
-//     }
-
-//     if (currentValue === "-") {
-//       return;
-//     }
-
-//   if (!currentValue.includes(".")) {
-//     currentValue += ".";
-//     $display.value = currentValue;
-//   }
-// });
-
-
-import './style.css'
 
 // ディスプレイの要素の取得
 const $display = document.querySelector("#display") as HTMLInputElement;
@@ -301,34 +53,73 @@ let justCBtn: boolean = false;
 // 数字を押したときの処理
 function inputNumber(num: string): void {
   const numOnlyLen = currentValue.replace(".", "").replace("-", "");
-  if (numOnlyLen.length >= 8) return;
-
+  if (numOnlyLen.length >= 8) 
+    return;
   if (currentValue === "0") {
-    currentValue = num;
+    currentValue = num; //初めて数字のボタンを押した時は０からそのボタンに変更する
     justCBtn = false;
   } else {
-    currentValue += num;
+    currentValue += num; //2回目以降は押したボタンを追加していく
   }
 
-  $display.value = currentValue;
+  $display.value = currentValue; //表示画面のを入力中にする
 }
 
-// 数字ボタンのイベント
-$btn1.addEventListener("click", () => inputNumber("1"));
-$btn2.addEventListener("click", () => inputNumber("2"));
-$btn3.addEventListener("click", () => inputNumber("3"));
-$btn4.addEventListener("click", () => inputNumber("4"));
-$btn5.addEventListener("click", () => inputNumber("5"));
-$btn6.addEventListener("click", () => inputNumber("6"));
-$btn7.addEventListener("click", () => inputNumber("7"));
-$btn8.addEventListener("click", () => inputNumber("8"));
-$btn9.addEventListener("click", () => inputNumber("9"));
-$btn0.addEventListener("click", () => inputNumber("0"));
+//各のボタンにイベントを割り当てる
+// 例：1を押した※文字列としての1を取得
+//※+=の処理を　11 のように実行するため (計算実行前にnumberへ変換は後に記載)
 
-// 演算処理
-function handleOperator(op: string): void {
+$btn1.addEventListener("click",() => {
+  inputNumber("1");
+});
 
-  if (justCBtn && op !== "-") return;
+$btn2.addEventListener("click",() => {
+  inputNumber("2")
+});
+
+$btn3.addEventListener("click",() => {
+  inputNumber("3")
+});
+
+$btn4.addEventListener("click",() => {
+  inputNumber("4")
+});
+
+$btn5.addEventListener("click",() => {
+  inputNumber("5")
+});
+
+$btn6.addEventListener("click",() => {
+  inputNumber("6")
+});
+
+$btn7.addEventListener("click",() => {
+  inputNumber("7")
+});
+
+$btn8.addEventListener("click",() => {
+  inputNumber("8")
+});
+
+$btn9.addEventListener("click",() => {
+  inputNumber("9")
+});
+
+$btn0.addEventListener("click",() => {
+  inputNumber("0")
+});
+
+//演算処理
+// ①previousValueが空白→currentValueを移す
+// ②両方ある場合→number型に変換し計算
+// ③operatorを更新
+// ④currentValueをリセット
+
+function handleOperator(op: string): any {
+
+  if (justCBtn && op !== "-") {
+    return;
+  }
 
   if (justCBtn && op === "-") {
     currentValue = "-";
@@ -336,7 +127,9 @@ function handleOperator(op: string): void {
     return;
   }
 
-  if (previousValue === null && currentValue === "0" && op !== "-") return;
+  if (previousValue === null && currentValue === "0" && op !== "-") {
+    return;
+  }
 
   if (previousValue === null && currentValue === "0" && op === "-") {
     currentValue = "-";
@@ -349,10 +142,12 @@ function handleOperator(op: string): void {
     return;
   }
 
-  if (previousValue !== null && currentValue !== "0") {
+    // previousValueが空白ではない　かつ　currentValueが0ではない場合の処理
+  if (previousValue !== null && currentValue !== "0") {        // 入力中に文字列としての数字と演算子を押す前の文字列の数字をnumberに変換
     const a = Number(previousValue);
     const b = Number(currentValue);
 
+  //演算子の処理
     if (operator === "+") previousValue = a + b;
     if (operator === "-") previousValue = a - b;
     if (operator === "*") previousValue = a * b;
@@ -361,16 +156,27 @@ function handleOperator(op: string): void {
     previousValue = Number(currentValue);
   }
 
-  operator = op;
-  currentValue = "0";
-  $display.value = String(previousValue);
+  operator = op;  //演算子は何かを記録
+  currentValue = "0"; //入力中の数字を０にしてリセット
+  $display.value = String(previousValue); //計算途中の結果を出す
 }
 
 // 演算子イベント
-$btnPlus.addEventListener("click", () => handleOperator("+"));
-$btnMinus.addEventListener("click", () => handleOperator("-"));
-$btnMultiple.addEventListener("click", () => handleOperator("*"));
-$btnDivide.addEventListener("click", () => handleOperator("/"));
+$btnPlus.addEventListener("click", () => {
+  handleOperator("+")
+});
+
+$btnMinus.addEventListener("click", () => {
+  handleOperator("-")
+});
+
+$btnMultiple.addEventListener("click", () => {
+  handleOperator("*")
+});
+
+$btnDivide.addEventListener("click", () => {
+  handleOperator("/")
+});
 
 // = の処理
 $btnEqual.addEventListener("click", () => {
@@ -397,9 +203,10 @@ $btnEqual.addEventListener("click", () => {
     if (operator === "*") result = a * b;
     if (operator === "/") result = a / b;
 
-    // 丸処理
-    // result = Math.round(result * 1e12) / 1e12;
+    // 丸め処理
+    result = Math.round(result * 1e12) / 1e12;
     
+    // 8桁以上の場合は指数表記にする
     const bigIntNum = String(result).replace(".", "").replace("-", "").length;
 
     if (bigIntNum > 8) {
@@ -430,11 +237,19 @@ $btnClear.addEventListener("click", () => {
 });
 
 // 小数点の処理
-$btnDot.addEventListener("click", () => {
+$btnDot.addEventListener("click",() =>{
 
-  if (justCBtn) return;
-  if (currentValue.includes(".")) return;
-  if (currentValue === "-") return;
+    if (justCBtn) {
+      return;
+    }
+
+    if(currentValue.includes(".")){
+      return;
+    }
+
+    if (currentValue === "-") {
+      return;
+    }
 
   if (!currentValue.includes(".")) {
     currentValue += ".";
